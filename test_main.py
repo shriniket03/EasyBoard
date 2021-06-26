fa4cc48ed364bad4ead1d6e7e83129750eb86cc2
Shanoop padmanabhan <shanoop@Shanoops-MacBook-Pro.local>
2021-06-26 13:21:16 +0800

    cleanup on aisle 9

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-from easyboard.main import app
+from main import app
 from config import DevConfig, ProdConfig
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '...')))
 
