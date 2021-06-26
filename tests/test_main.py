5aea56d864ed6a5e82c081186a679487d9c9dd90
Shanoop padmanabhan <shanoop@Shanoops-MacBook-Pro.local>
2021-06-26 12:41:06 +0800

    Cleanup rev 2

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-from Google_API_1.main import app
+from easyboard.main import app
 from config import DevConfig
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '...')))
 
